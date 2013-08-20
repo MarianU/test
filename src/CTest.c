974610b37d102b0ac332b9945c84c9b09ec061a2
marian <Ubuntu.localMail>
2013-08-20 11:40:11 +0300

    Primary commit

---
+++ src/CTest.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : CTest.c
+ Author      : MarianU
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	puts("Hello World\n"); /* prints Hello World */
+	printf("This is just a test of printf %d\n", 1.2); /* prints whatever is written between quotes :D */
+
+	return EXIT_SUCCESS;
+}
